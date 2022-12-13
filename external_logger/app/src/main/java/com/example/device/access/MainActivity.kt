4f3ac5c0a4f94363331f5c763a80eedbca791282
MinJun <68832065+wkdalswns0427@users.noreply.github.com>
2022-12-13 17:42:47 +0900

    updated version

--- external_logger/app/src/main/java/com/example/device/access/MainActivity.kt
+++ external_logger/app/src/main/java/com/example/device/access/MainActivity.kt
@@ -1,6 +1,9 @@
 package com.example.device.access
 
 import android.app.Activity
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
 import android.os.Bundle
 import android.os.Environment
 import android.os.Handler
@@ -15,141 +18,18 @@ import java.util.*
 //input saver
 //-------------------------------------------------------------------------------------------------
 class MainActivity : Activity() {
-    var inputText: EditText? = null
-    var response: TextView? = null
-    lateinit var saveButton: Button
-    lateinit var readButton: Button
-    lateinit var writeButton: Button
-    private val filename = "SampleFile.txt"
-    private val filepath = "MyFileStorage"
-    var myExternalFile: File? = null
-    var myData = ""
-    var timer: Timer = Timer()
+
     private val mDelayHandler: Handler by lazy {
         Handler()
     }
+    val ext = externalLogWriter(this)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(com.example.device.access.R.layout.sample_layout)
 
-        inputText = findViewById<View>(R.id.myInputText) as EditText?
-        response = findViewById<View>(R.id.response) as TextView?
-        saveButton = findViewById<Button>(com.example.device.access.R.id.saveExternalStorage)
-        readButton = findViewById<Button>(com.example.device.access.R.id.getExternalStorage)
-        writeButton = findViewById<Button>(com.example.device.access.R.id.saveLogExt)
 
-        leaveLogTimer()
-        saveButton.setOnClickListener {
-            try {
-//                val fos = FileOutputStream(myExternalFile)
-//                fos.write(inputText?.text.toString().toByteArray())
-//                fos.close()
-                Log.d("buttonTag_d","saveButton click " + System.currentTimeMillis() )
-                Log.i("buttonTag_e","saveButton info " + System.currentTimeMillis() )
-                Log.w("buttonTag_w","saveButton info " + System.currentTimeMillis() )
+        ext.leaveLogTimer()
 
-            } catch (e: IOException) {
-                e.printStackTrace()
-            }
-            inputText?.setText("")
-            response?.text = "SampleFile.txt saved to External Storage..."
-        }
-
-        readButton.setOnClickListener {
-            try {
-                val fis = FileInputStream(myExternalFile)
-                val `in` = DataInputStream(fis)
-                val br = BufferedReader(InputStreamReader(`in`))
-                var strLine: String
-                while (br.readLine().also { strLine = it } != null) {
-                    myData = myData + strLine
-                }
-                `in`.close()
-
-            } catch (e: IOException) {
-                e.printStackTrace()
-            }
-            inputText?.setText(myData)
-            response?.text = "SampleFile.txt data retrieved from Internal Storage..."
-        }
     }
-
-    private fun leaveLog(){
-        val c = Calendar.getInstance()
-
-        val year = c.get(Calendar.YEAR)
-        val month = c.get(Calendar.MONTH)
-        val day = c.get(Calendar.DAY_OF_MONTH)
-
-        val filename = "logfile" + year + month + day + ".txt"
-        Log.d("LogTag","This is timer " + System.currentTimeMillis() )
-        val outputFile = File(this?.externalCacheDir, filename)
-        Runtime.getRuntime().exec(  "logcat -v threadtime -d *:V -f"+outputFile.absolutePath)
-        leaveLogTimer()
-    }
-
-    // recursive finction running inside
-    private fun leaveLogTimer(){
-        mDelayHandler.postDelayed(::leaveLog, 10000) // 1 min delay
-    }
-
 }
-//-------------------------------------------------------------------------------------------------
-// these used to be inside onCreate
-//        saveButton.setOnClickListener {
-//            try {
-//                val fos = FileOutputStream(myExternalFile)
-//                fos.write(inputText?.text.toString().toByteArray())
-//                fos.close()
-//            } catch (e: IOException) {
-//                e.printStackTrace()
-//            }
-//            inputText?.setText("")
-//            response?.text = "SampleFile.txt saved to External Storage..."
-//        }
-//
-//        readButton.setOnClickListener {
-//
-////            try {
-////                val fis = FileInputStream(myExternalFile)
-////                val `in` = DataInputStream(fis)
-////                val br = BufferedReader(InputStreamReader(`in`))
-////                var strLine: String
-////                while (br.readLine().also { strLine = it } != null) {
-////                    myData = myData + strLine
-////                }
-////                `in`.close()
-////
-////            } catch (e: IOException) {
-////                e.printStackTrace()
-////            }
-////            inputText?.setText(myData)
-////            response?.text = "SampleFile.txt data retrieved from Internal Storage..."
-//            leaveLog()
-//        }
-//
-//
-//
-//        if (!MainActivity.Companion.isExternalStorageAvailable || MainActivity.Companion.isExternalStorageReadOnly) {
-//            saveButton.isEnabled = false
-//        } else {
-//            myExternalFile = File(getExternalFilesDir(filepath), filename)
-//        }
-//    }
-//
-//    companion object {
-//        private val isExternalStorageReadOnly: Boolean
-//            get() {
-//                val extStorageState = Environment.getExternalStorageState()
-//                return if (Environment.MEDIA_MOUNTED_READ_ONLY == extStorageState) {
-//                    true
-//                } else false
-//            }
-//        private val isExternalStorageAvailable: Boolean
-//            get() {
-//                val extStorageState = Environment.getExternalStorageState()
-//                return if (Environment.MEDIA_MOUNTED == extStorageState) {
-//                    true
-//                } else false
-//            }
--- external_logger/app/src/main/java/com/example/device/access/externalLogWriter.kt
+++ external_logger/app/src/main/java/com/example/device/access/externalLogWriter.kt
@@ -1,18 +1,59 @@
 package com.example.device.access
 
 import android.content.Context
-import android.util.Log
+import android.os.Environment
+import android.os.Handler
+import java.io.DataOutputStream
 import java.io.File
+import java.io.FileOutputStream
+import java.text.SimpleDateFormat
 import java.util.*
 
+
 class externalLogWriter(private var context: Context) {
+    var timer: Timer = Timer()
+    private val mDelayHandler: Handler by lazy {
+        Handler()
+    }
+
+    // append to external logfile 'customlogfileyymmdd.txt'
     fun extLogger(tag: String,content: String){
         val c = Calendar.getInstance()
         val year = c.get(Calendar.YEAR)
-        val month = c.get(Calendar.MONTH)
+        val month = c.get(Calendar.MONTH) + 1
         val day = c.get(Calendar.DAY_OF_MONTH)
 
-        val filename = "logfile" + year + month + day + ".txt"
+
+        val formattedDate = SimpleDateFormat("yyyy.MM.dd HH:mm:ss",Locale.ENGLISH).format(c.timeInMillis)
+        val logdata  = formattedDate + "\t" + tag + "\t" + content + "\n"
+
+
+        val filename = "customlogfile$year$month$day.txt"
         val outputFile = File(context?.externalCacheDir, filename)
+        outputFile.appendText(logdata)
+
+
     }
+
+    //----------------------------------------------------------------------------------------------------------------
+    fun leaveLog(){
+        val c = Calendar.getInstance()
+
+        val year = c.get(Calendar.YEAR)
+        val month = c.get(Calendar.MONTH) + 1
+        val day = c.get(Calendar.DAY_OF_MONTH)
+
+        val filename = "logfile$year$month$day.txt"
+        val outputFile = File(context?.externalCacheDir, filename)
+
+        Runtime.getRuntime().exec(  "logcat -v threadtime -d *:V -f"+outputFile.absolutePath)
+        extLogger("custom", "custom external log")
+        leaveLogTimer()
+    }
+
+    // recursive function running inside
+    fun leaveLogTimer(){
+        mDelayHandler.postDelayed(::leaveLog, 60000) // 1 min delay
+    }
+    //----------------------------------------------------------------------------------------------------------------
 }
